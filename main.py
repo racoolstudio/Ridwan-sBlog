504ac5885c3947dc2f3d3f6e2a402c9436fe37ff
racoolstudio <75172422+racoolstudio@users.noreply.github.com>
2023-04-27 07:37:47 -0230

    #1

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-import time
-from datetime import datetime
+# importing flask
+# we using jinja as the templating language
+from requests import *
 from flask import Flask, render_template
 from random import randint
-import requests
+from datetime import date
+from requests import request
 
+# creating an instance
 app = Flask(__name__)
-# you can write some python code in html using {{whatever}}
-currentYear = datetime.now().year
-blog_api = 'https://api.npoint.io/bf5d53a77828a9cc7b35'
-blog_data = requests.get(blog_api).json()
+
+# api for blog
+blogApi = "https://api.npoint.io/c5a0ec9d3ffe21184c23"
+blogData = get(blogApi).json()
+
+# current year
+currentYear = date.today().year
 
 
+# the root's route
 @app.route('/')
-def home():
-    return render_template('index.html', greet="Hello World 👋🏻", year=currentYear, data=blog_data)
+# so basically the route is where the function is located
+
+# route for blog
+@app.route("/")
+# function for Blog
+def startBlog():
+    return render_template("index.html", data=blogData, year=currentYear)
 
 
-@app.route('/blog/<int:num>')
-def get_blog(num):
-    return render_template('blog.html', data=blog_data, id=num)
+# function for blog details
+@app.route("/blog/<num>")
+def loadBlog(num):
+    return render_template("blog.html", id=num, data=blogData, year=currentYear)
 
 
+# checks for the main function
 if __name__ == "__main__":
     app.run(debug=True)
